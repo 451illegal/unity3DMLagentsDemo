639a206c9a7b00309da33cc9df307eb50e3c785a
451illegal <mrmaisg@163.com>
2018-11-19 18:44:25 +0800

    basic example from Unity and indoor scene pedestrian training

---
+++ Assets/Scripts/RollerAgent.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class RollerAgent : Agent {
+       
+    Rigidbody rB;
+
+	// Use this for initialization
+	void Start () {
+		//get the rigidBody
+        rB = GetComponent<Rigidbody>();
+	}
+	
+    //get the target information
+    public Transform target;
+    
+    /// <summary>
+    /// //Agent重置函数
+    /// 基本逻辑：当小球落地时，重置小球到平面上；
+    /// 小球到达目标时会将自己标记为完成状态，而 agent 重置函数会将目标移动到随机位置
+    /// </summary>
+    public override void AgentReset()
+    {
+        //小球掉落平面
+        if (this.transform.position.y < -1.0f)
+        {
+            //reset the position
+            this.transform.position = Vector3.zero;
+            //reset the rigidbody
+            this.rB.angularVelocity = Vector3.zero;
+            this.rB.velocity = Vector3.zero;
+        }
+        else
+        {
+            //随机移动target的位置
+            target.position = new Vector3(Random.value * 8 - 4,
+                                            0.5f,
+                                            Random.value * 8 - 4);
+        }
+    }
+
+    /// <summary>
+    /// 收集观测信息
+    /// </summary>
+    List<float> observations = new List<float>();
+    public override void CollectObservations()
+    {
+        //计算小球与目标的相对位置
+        Vector3 relativePos = target.transform.position - this.transform.position;
+
+        //添加观测数据(除以5为了使输入的值在[-1,1]范围内)
+        AddVectorObs(relativePos.x / 5);
+        AddVectorObs(relativePos.z / 5);
+        //小球与平台边缘之间的距离
+        AddVectorObs((this.transform.position.x + 5) / 5);
+        AddVectorObs((this.transform.position.x - 5) / 5);
+        AddVectorObs((this.transform.position.z + 5) / 5);
+        AddVectorObs((this.transform.position.z - 5) / 5);
+        //Agent的速度(有助于agent学习控制速度不至于太快滑落)
+        AddVectorObs(this.rB.velocity.x / 5);
+        AddVectorObs(this.rB.velocity.z / 5);
+
+    }
+
+    public float speed = 10f;
+    private float previousDistance = float.MaxValue;
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        //通过与目标间的距离设置奖励
+        float distanceToTarget = Vector3.Distance(this.transform.position, target.position);
+
+        if (distanceToTarget < 1.42f)
+        {
+            //已到达目标,将agent标记为已完成状态
+            Done();
+            AddReward(1.0f);
+        }
+        if (distanceToTarget < previousDistance)
+        {
+            AddReward(0.1f);
+        }
+        //时间惩罚
+        AddReward(0.05f);
+        //小球掉落平台惩罚
+        if (this.transform.position.y < -1.0f)
+        {
+            Done();
+            AddReward(-1.0f);
+        }
+        previousDistance = distanceToTarget;
+
+        //rigidbody move
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = Mathf.Clamp(vectorAction[0],-1,1);
+        controlSignal.z = Mathf.Clamp(vectorAction[1],-1,1);
+        rB.AddForce(controlSignal * speed);
+    }
+
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/WAgent.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class WAgent : Agent {
+
+    public Transform target;
+    CharacterController player;
+
+    public float moveSpeed;
+    public float rotationSpeed;
+
+    RayPerception rayPer;
+
+    //initialization
+    void Start()
+    {
+        //get the character controller
+        player = GetComponent<CharacterController>();
+        rayPer = GetComponent<RayPerception>();
+    }
+
+    /// <summary>
+    /// 重置agent   !!!ATTENTION
+    /// 基本逻辑：当agent的位置到达target位置(之间的距离小于一定的阈值时)的时候，随机改变target的位置?移动agent的位置
+    /// </summary>
+    public override void AgentReset()
+    {
+        if (Mathf.Abs(this.transform.position.x - target.position.x) < 1f || Mathf.Abs(this.transform.position.z - target.position.z)< 1f)
+        {
+            //随机改变agent的位置
+            this.transform.position = new Vector3(Random.value*400 - 400,
+                                                  15,              
+                                                  Random.value*200 - 200);
+            
+        }
+    }
+
+    /// <summary>
+    /// 收集agent观测信息 TODO:
+    /// </summary>
+    List<float> observations = new List<float>();
+    public override void CollectObservations()
+    {
+        //使用raycast进行观察,观察标签为wall与obs的障碍
+        float raycastDistance = 5f;
+        float[] rayAngles = { 20f, 90f, 160f, 45f, 135f, 70f, 110f };
+        string[] detectableObjects = { "wall", "target" ,"obs"};
+        AddVectorObs(rayPer.Perceive(raycastDistance,rayAngles,detectableObjects,0f,-0.1f));
+
+        //agent的位置(在xoz平面上的位置)并归一化
+        AddVectorObs((this.transform.position.x + 40) / (-440));
+        AddVectorObs((this.transform.position.z + 40) / 260);
+
+        //agent进行绕y轴旋转
+        AddVectorObs(this.transform.rotation.y);
+    }
+    /// <summary>
+    /// 设置奖励，并返回agent动作
+    /// </summary>
+    private float previousDistance = float.MaxValue;
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        //计算player与target之间的距离
+        float distanceToTarget = Vector3.Distance(this.transform.position,target.transform.position);
+        if (distanceToTarget < 1.5f)
+        {
+            //已到达目标,将agent标记为已完成状态
+            Done();
+            AddReward(1.0f);
+        }
+        if (distanceToTarget < previousDistance)
+        {
+            AddReward(0.1f);
+        }
+
+        //agent action
+        Vector3 dirToGo = Vector3.zero;
+        Vector3 rotateDir = Vector3.zero;
+        if (brain.brainParameters.vectorActionSpaceType == SpaceType.continuous)
+        {
+            dirToGo = transform.forward * Mathf.Clamp(vectorAction[0], -1, 1);
+            rotateDir = transform.up * Mathf.Clamp(vectorAction[1], -1, 1);
+        }
+        else
+        {
+            var forwardAxis = (int)vectorAction[0];
+            var rightAxis = (int)vectorAction[1];
+            var rotateAxis = (int)vectorAction[2];
+            switch (forwardAxis)
+            {
+                case 1:
+                    dirToGo = transform.forward;
+                    break;
+                case 2:
+                    dirToGo = -transform.forward;
+                    break;
+            }
+
+            switch (rightAxis)
+            {
+                case 1:
+                    dirToGo = transform.right;
+                    break;
+                case 2:
+                    dirToGo = -transform.right;
+                    break;
+            }
+            switch (rotateAxis)
+            {
+                case 1:
+                    rotateDir = -transform.up;
+                    break;
+                case 2:
+                    rotateDir = transform.up;
+                    break;
+            }
+        }
+
+        // Time penalty
+        AddReward(-0.005f);
+        
+    }
+
+    //rayperception使用tag碰撞检测 TODO
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("wall") || collision.gameObject.CompareTag("obs"))
+        {
+            AddReward(-0.02f);
+        }
+    }
+
+    public override void AgentOnDone()
+    {
+        
+    }
+	
+}
